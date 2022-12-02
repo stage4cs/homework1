3195803c7b13ac55e87c044a8eb26e516f67cad0
stage4cs <119388245+stage4cs@users.noreply.github.com>
2022-12-02 20:06:46 +0300

    Add files via upload

---
+++ حيدر محمد نصيف.py
@@ -0,0 +1,10 @@
+ 
+import winsound
+filename='yaqeen'
+winsound.playsound(filename,winsound.Son.filename)
+
+"عند التشفير بواسطة
+"matrix
+"فيتغير النص بتغير
+"key
+"الموجود
---
+++ محمد سلام شمران.py
@@ -0,0 +1,8 @@
+ 
+from playsound import playsound
+playsound.(اسم الملف.mp3)
+
+"عند التشفير بواسطة
+"matrix
+"فيتغير النص بتغير
+"key
