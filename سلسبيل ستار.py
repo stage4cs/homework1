5ccb426b2bff2441c8d077de58c7de50beaf9da8
stage4cs <119388245+stage4cs@users.noreply.github.com>
2022-12-02 19:52:36 +0300

    Add files via upload

---
+++ سلسبيل ستار.py
@@ -0,0 +1,26 @@
+from Scipy.io import wavfile
+from ipython.display import Audio
+from matplot.pyplot import pyplotasplt
+f_name="/contant/drive/MyDrive/myDrive/MLPROJECTS/SpeechRecognition/0a7c2a8d_nahash_0.Wav"
+fs,wav=wavfile.read(f_name)
+print(fs)
+plt.plot(wav)
+Audio(f_name,autoplay=true)
+
+     "َQ2
+     "لمن الشفرة كلمة بل
+     "key
+     "مثل 5راح يطلع كلمة مشفرة لمن اريد فك الشفرة بس
+     "key
+     "مختلف مو مثل المفتاح الي بل التشفير يتم استخدام الارقام 
+     "0-25
+     "حتى اشوف اي رقم من ارقام حتى يطلع نص مفهوم فما اعتقد 
+     "0-25
+     "ما يطلع كلمة مفهومة الا استخدام نفس
+     "key
+     "الي تتم استخدام الى التشفير
+     
+ 
+
+
+
